c535bd85c81cba90a21d833e795af85b2d6aeb05
Yure Galdino Lopes <yuregaldino@hotmail.com>
2024-01-26 18:08:47 -0300

    fix: added dotenv

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,15 +5,15 @@ dotenv.config();
 const { DATABASE_URL, SALT, PORT, DB_URI, DB_NAME } = process.env;
 
 const getConfiguration = () => ({
-    dbUrl: DATABASE_URL,
-    salt: parseInt(SALT, 10),
-    port: parseInt(PORT, 10),
+    dbUrl: DATABASE_URL || 'mongodb://localhost:27017/games-score',
+    salt: parseInt(SALT, 10) || 10,
+    port: parseInt(PORT, 10) || 3000,
     app: {
         prefix: '/',
     },
     dbConfig: {
-        dbUri: DB_URI,
-        dbName: DB_NAME,
+        dbUri: DB_URI || 'mongodb://localhost:27017',
+        dbName: DB_NAME || 'games-score',
     },
 });
 
