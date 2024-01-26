559994ae258bd8075e3d1a457d7c77cab33f8b54
Yure Galdino Lopes <yuregaldino@hotmail.com>
2024-01-26 17:36:56 -0300

    chore: added new configs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,19 @@
-const { DATABASE_URL, SALT, PORT } = process.env;
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const { DATABASE_URL, SALT, PORT, DB_URI, DB_NAME } = process.env;
 
 const getConfiguration = () => ({
     dbUrl: DATABASE_URL,
-    salt: SALT,
-    port: PORT,
+    salt: parseInt(SALT, 10),
+    port: parseInt(PORT, 10),
     app: {
         prefix: '/',
+    },
+    dbConfig: {
+        dbUri: DB_URI,
+        dbName: DB_NAME,
     },
 });
 
